d30110b17a68d5b262bae881d6bf6c3bddf4c225
albiamala <120713863+albiamala@users.noreply.github.com>
2023-01-17 11:49:11 +0530

    Add files via upload

---
+++ Question2.cs
@@ -0,0 +1,32 @@
+﻿//QUESTION 2 :- ACCEPT FLIGHT FARE AND BOOKING TIME
+
+Console.WriteLine("Enter the booking time : ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the flight fare : ");
+double fare = Convert.ToDouble(Console.ReadLine());
+
+   if (time > 6 && time < 9)
+   {
+       fare += (fare * 0.1);
+       Console.WriteLine(fare);
+   }
+   else if (time > 9 & time <= 17)
+   {
+       fare += (fare * 0.2);
+       Console.WriteLine(fare);
+   }
+   else if (time > 17 & time <= 23)
+   {
+       fare += (fare * 0.07);
+       Console.WriteLine(fare);
+   }
+   else if (time > 24)
+   {
+       Console.WriteLine("Not Valid!");
+   }
+   else
+   {
+       fare += (fare * 0.05);
+       Console.WriteLine(fare);
+   }
