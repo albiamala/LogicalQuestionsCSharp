45e161656c9349b46112a61028651c66a2f42493
albiamala <120713863+albiamala@users.noreply.github.com>
2023-01-19 12:45:51 +0530

    Add files via upload

---
+++ fareAndDestination.cs
@@ -0,0 +1,39 @@
+﻿//ACCEPT FLIGHT FARE AND DESTINATIONS
+
+ 
+   //array to accept destination
+    String[] Destination = new string[5];
+    
+    for (int i = 0; i < Destination.Length; i++)
+    {
+        Console.WriteLine("Enter the destination : ");
+        Destination[i] = Console.ReadLine();
+    }
+    
+ 
+   //array to accept flightfare  
+    Double[] flightFare = new double[5];
+    
+    for (int i = 0; i < flightFare.Length ; i++)
+    {
+        Console.WriteLine("Enter flight fares: ");
+        flightFare[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    
+    
+    Console.WriteLine("Enter nth number to display corresponding destination and fare: ");
+    int index = int.Parse(Console.ReadLine());
+    
+    if (index > Destination.Length)
+    {
+        Console.WriteLine("Enter valid number");
+    }
+    
+    else
+    {
+        Console.WriteLine($" Destination : {Destination[index]} | Fare : {flightFare[index]}");
+    }
+    
+    
+    
+    
